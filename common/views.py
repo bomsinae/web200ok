5abfc3949d20d5c2f3758a96b1e08b1eed5a9a35
Kim Hyunsu <kimhyunsu@gmail.com>
2025-04-25 16:42:28 +0900

    메인 페이지 및 URL 모니터링 기능 추가

--- common/views.py
+++ common/views.py
@@ -4,6 +4,39 @@ from django.contrib.auth.models import User
 from django.views.generic import ListView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import SignUpForm, UserUpdateForm
+from monitor.models import Http, HttpResult
+
+
+def main(request):
+    """
+    비상정적인 페이지와 꺼저있는 페이지 보여주기.
+    """
+
+    # 각 URL의 마지막 모니터링 결과를 가져오기 위한 쿼리
+    from django.db.models import Max, Subquery, OuterRef
+
+    # 각 HTTP URL별 가장 최근 체크 시간 구하기
+    latest_checks = HttpResult.objects.filter(
+        http=OuterRef('http')
+    ).order_by('-checked_at').values('id')[:1]
+
+    # 위에서 구한 최근 체크 결과만 가져오기
+    latest_results = HttpResult.objects.filter(
+        id__in=Subquery(latest_checks)
+    )
+
+    # 그 중에서 status가 'success'가 아닌 것만 필터링
+    http_results = latest_results.exclude(status='success')
+
+    # 모니터링이 꺼져 있는 URL을 가져오기
+    http_off_list = Http.objects.filter(is_active=False)
+
+    context = {
+        'http_results': http_results,
+        'http_off_list': http_off_list,
+    }
+
+    return render(request, 'common/main.html', context)
 
 
 def signup_success(request):
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from common.views import main
 
 urlpatterns = [
+    path('', main, name='main'),
     path('admin/', admin.site.urls),
     path('common/', include('common.urls', namespace='common')),
     path('monitor/', include('monitor.urls', namespace='monitor')),
