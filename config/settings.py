e50aef22a470d8b7643d1b9ca0e3e7d095f2df93
Kim Hyunsu <kimhyunsu@gmail.com>
2025-05-13 16:13:57 +0900

    로깅 핸들러를 RotatingFileHandler에서 WatchedFileHandler로 변경

--- config/settings.py
+++ config/settings.py
@@ -193,7 +193,7 @@ LOGGING = {
         },
         'file': {
             'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
+            'class': 'logging.handlers.WatchedFileHandler',
             'filename': BASE_DIR / 'logs/server.log',
             'maxBytes': 1024*1024*5,  # 5 MB
             'backupCount': 5,
